9b081b594c655b9d57230a13af85ab566e8102e4
Mihail Pancenco <mihailpancenco@gmail.com>
2024-05-13 15:49:48 +0300

    vite as nightmare

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -266,43 +266,13 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-
-
-        //Post::factory()->has(Image::factory()->count(rand(1, 15)))->count(20)->create();
-
-        /*for ($i = 0; $i < 20; $i++) {
-            $post = Post::factory()->create();
-            Image::factory(rand(1, 15))->create(['post_id' => $post->id]);
-        }*/
-
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 5; $i++) {
             $user = User::factory()->create();
             for ($j = 0; $j<7;$j++) {
                 $post = Post::factory()->create(['user_id' => $user->id]);
                 Image::factory(rand(1, 5))->create(['post_id' => $post->id]);
             }
         }
-
-
-        /*
-         *
-         * for ($i = 0; $i < 10; $i++) {
-            $user = User::factory()->create();
-            for ($j = 0; $j<5;$j++){
-                $post = Post::factory(rand(1, 5))->create(['user_id' => $user->id]);
-                Image::factory(rand(1, 5))->create(['post_id' => $post->id]);
-            }
-
-        }
-         *
-         * */
-
-        /*$posts = Post::factory(20)->create();
-        $images = Image::factory(100)->create();
-        foreach ($images as $image){
-            $postsIds = $posts->random(5)->pluck('id');
-            $image->post()->attach($postsIds);
-        }*/
 
 
     }
